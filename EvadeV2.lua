5df562feeb383f3fd14bc73a7cf2b4798eb3c36f
bbrw706 <hnung7071@gmail.com>
2026-02-22 16:45:23 +0700

    Update EvadeV2.lua

--- EvadeV2.lua
+++ EvadeV2.lua
@@ -1,5 +1,5 @@
 -- =========================
--- Load UI Library ตอนนี้ถึง ปุ่มลอย Floating Button
+-- Load UI Library ตอนนี้ถึง สร้าง Slider ใน SettingsTab
 -- =========================
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/x2zu/OPEN-SOURCE-UI-ROBLOX/refs/heads/main/X2ZU%20UI%20ROBLOX%20OPEN%20SOURCE/DummyUi-leak-by-x2zu/fetching-main/Tools/Framework.luau"))()
 
